f4ded4fcb9d298fb6ce07fd279f80eb21afb34ab
Gabriela Martínez <gabriela.martinez@estudiantes.utec.edu.uy>
2024-04-12 21:15:20 -0300

    Agora abre mesmo

--- app/src/main/java/com/example/acreditads2023/Consulta_id.java
+++ app/src/main/java/com/example/acreditads2023/Consulta_id.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import androidx.appcompat.app.AppCompatActivity;
-
 public class Consulta_id extends AppCompatActivity {
     Organizadores org;
     Participantes part;
