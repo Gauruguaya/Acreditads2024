90286633deec0017f627951834ec47d5008c3f8c
FabricioQv <fabricioquevedo.sl003@academico.ifsul.edu.br>
2024-06-14 22:22:43 -0300

    Adição das partes de Agustin e Fabricio Aloy

--- app/src/main/java/com/example/acreditads2023/Evento_activity.java
+++ app/src/main/java/com/example/acreditads2023/Evento_activity.java
@@ -1,13 +1,25 @@
 package com.example.acreditads2023;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -20,15 +32,22 @@ public class Evento_activity extends AppCompatActivity {
     private EditText edtTipoEvento;
     private EditText edtFechaHoraEvento;
     private EditText edtIdAdminEvento;
-    //private EditText edtQRaperturaEvento;
-    //private EditText edtQRcierreEvento;
+    private EditText edtQRaperturaEvento;
+    private EditText edtQRcierreEvento;
     private Button btnAgregar;
     private Button btnVolver;
     private Database db;
-    Button btnMapa;
+    private Button btnMapa;
+
+    private static final String CHANNEL_ID = "canal";
+
+    private static final int REQUEST_NOTIFICATION_PERMISSION = 1;
+
     private RequestQueue requestQueue;
-    String latitude;
-    String longitude;
+
+    private String Latitud,Longitud;
+    private static final int REQUEST_CODE = 1;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +61,9 @@ public class Evento_activity extends AppCompatActivity {
         edtTipoEvento = findViewById(R.id.edtTipoEvento);
         edtFechaHoraEvento = findViewById(R.id.edtFechaHoraEvento);
         edtIdAdminEvento = findViewById(R.id.edtIdAdminEvento);
-       // edtQRaperturaEvento = findViewById(R.id.edtQRaperturaEvento);
-        //edtQRcierreEvento = findViewById(R.id.edtQRcierreEvento);
-
         db = new Database(getApplicationContext());
+
+        Toast.makeText(getApplicationContext(),"La latitud es: " + Latitud + ", Y la Longitud es: " + Longitud,Toast.LENGTH_SHORT).show();
 
         btnAgregar = findViewById(R.id.btnAgregar);
         btnAgregar.setOnClickListener(new View.OnClickListener() {
@@ -61,11 +79,14 @@ public class Evento_activity extends AppCompatActivity {
                 EventoDAO eDAO = new EventoDAO(db);
                 eDAO.salvar(e);
 
+                // Llamar al método verificarPermisoNotificaciones() después de que el usuario se registre con éxito
+                verificarPermisoNotificaciones();
+
                 // Google Sheets URL
                 //Eu preciso puxar as variáveis aqui
                 String urlSheets = "https://script.google.com/macros/s/AKfycbxX1EInivW85EH1IGNZzh6bWJTLm6K9-ZRr3-wduHYFd34d1SW0dG2bAndm8CZvYPE/exec?action=evento&tituloEvento=" +
                         edtTituloEvento.getText().toString() + "&autorEvento=" + edtAutorEvento.getText().toString() +
-                        "&tipoEvento=" + edtTipoEvento.getText().toString() + "&fechaHoraEvento=" + edtFechaHoraEvento.getText().toString() + "&latitud=" + latitude + "&longitud=" + longitude;
+                        "&tipoEvento=" + edtTipoEvento.getText().toString() + "&fechaHoraEvento=" + edtFechaHoraEvento.getText().toString() + "&latitud=" + Latitud + "&longitud=" + Longitud;
 
                 StringRequest stringRequest = new StringRequest(Request.Method.GET, urlSheets, new com.android.volley.Response.Listener<String>() {
                     @Override
@@ -76,6 +97,7 @@ public class Evento_activity extends AppCompatActivity {
                     @Override
                     public void onErrorResponse(com.android.volley.VolleyError error) {
                         Log.e("Error", error.toString());
+                        Toast.makeText(getApplicationContext(), "Error al crear Evento", Toast.LENGTH_SHORT).show();
                     }
                 });
 
@@ -99,20 +121,101 @@ public class Evento_activity extends AppCompatActivity {
                 Intent intent;
                 intent = new Intent(Evento_activity.this, UbicarEventoActivity.class);
                 intent.putExtra("NombreEvento",nombreEvento);
-                startActivity(intent);
+                startActivityForResult(intent,REQUEST_CODE);
             }
         });
     }
+    //################################### AFUERA DEL onCreate #######################################
+
+    //###########################################################################################
+    // Recibe valores Latitud y Longitud
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (resultCode == RESULT_OK) {
             // Obtener los valores de las variables devueltas
-            latitude = data.getStringExtra("Latitud");
-            longitude = data.getStringExtra("Longitud");
-            Log.i("Latitud",latitude);
-            Log.i("Longitud",longitude);
+            String LatitudAux = data.getStringExtra("Latitud");
+            String LongitudAux = data.getStringExtra("Longitud");
+
+            // VARIABLES QUE CONTIENEN EL VALOR DEL EVENTO
+            Latitud = LatitudAux;
+            Longitud = LongitudAux;
         }
+    }
+    //###########################################################################################
+
+    //###########################################################################################
+    private void verificarPermisoNotificaciones() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            // Verificar si el permiso de notificación está concedido
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                // Solicitar permiso de notificación
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, REQUEST_NOTIFICATION_PERMISSION);
+            } else {
+                // Permiso ya concedido
+                Toast.makeText(this, "Permisos de notificación ya concedidos", Toast.LENGTH_SHORT).show();
+                configurarCanalNotificacion();
+                enviarNotificacion();
+            }
+        } else {
+            // No es necesario solicitar permisos en versiones anteriores a Android 13
+            configurarCanalNotificacion();
+            enviarNotificacion();
+        }
+    }
+    //###########################################################################################
+
+    //###########################################################################################
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_NOTIFICATION_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permiso concedido, configurar el canal de notificación
+                configurarCanalNotificacion();
+                enviarNotificacion();
+            } else {
+                // Permiso denegado
+                Toast.makeText(this, "Permiso de notificación denegado", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    //###########################################################################################
+
+    //###########################################################################################
+    private void configurarCanalNotificacion() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                    "Canal de notificaciones", NotificationManager.IMPORTANCE_DEFAULT);
+            // Configurar otras propiedades del canal, si es necesario
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+    }
+    //###########################################################################################
+
+    //###########################################################################################
+    private void enviarNotificacion() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notification) // Icono de la notificación
+                .setContentTitle("Creacion de Usuario")
+                .setContentText("Usuario creado con exito!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        notificationManager.notify(1, builder.build());
+        //###########################################################################################
     }
 }
