437d27d3b9a6245039c94ea7f2f02b8395eb3141
FabricioQv <fabricioquevedo.sl003@academico.ifsul.edu.br>
2024-07-03 23:18:38 -0300

    Redirecionamento para evento_activity

--- app/src/main/java/com/example/acreditads2023/AccesoAdm_activity.java
+++ app/src/main/java/com/example/acreditads2023/AccesoAdm_activity.java
@@ -1,23 +1,27 @@
 package com.example.acreditads2023;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -32,14 +36,13 @@ public class AccesoAdm_activity extends AppCompatActivity {
     private Button btnVolver;
     private static final String CHANNEL_ID = "canal";
     private static final int REQUEST_NOTIFICATION_PERMISSION = 1;
-    private AccesoAdministradoresDAO guardarTarea;
-    private Database db;
-
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private String idInstalacion;
     private RequestQueue requestQueue;
 
     @Override
-    protected void onCreate(Bundle savedInstanceStare) {
-        super.onCreate(savedInstanceStare);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.acceso_administradores);
 
         edtClaveAdm = findViewById(R.id.edtClaveAdm);
@@ -49,56 +52,72 @@ public class AccesoAdm_activity extends AppCompatActivity {
 
         requestQueue = Volley.newRequestQueue(this);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            idInstalacion = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        } else {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, PERMISSION_REQUEST_CODE);
+            } else {
+                initializeIdInstalacion();
+            }
+        }
+
         btnAgregar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String claveAdm = edtClaveAdm.getText().toString();
                 String tareaOrg = edtTareaOrg.getText().toString();
-                guardarTarea = new AccesoAdministradoresDAO(db);
-                guardarTarea.salvar(new AccesoAdministradores(tareaOrg));
 
                 // URL para verificar la clave en Google Sheets
-                String urlVerificarClave = "https://script.google.com/macros/s/AKfycbxX1EInivW85EH1IGNZzh6bWJTLm6K9-ZRr3-wduHYFd34d1SW0dG2bAndm8CZvYPE/exec?action=verificarClave&clave=" + claveAdm;
+                String urlVerificarClave = "https://script.google.com/macros/s/AKfycbwRHeHUHMtCXLe-iyzuVj6MXrxaW-zl5-Q-g8h_yHWYAm8KI9jDwaKcFINKpyX-eA/exec?action=verificarClave&clave=" + claveAdm;
 
                 StringRequest verificarClaveRequest = new StringRequest(Request.Method.GET, urlVerificarClave, new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        if (response.equals("VALIDO")) {
-                            // Si la clave es válida, guardar la tarea en Google Sheets
-                            String urlGuardarTarea = "https://script.google.com/macros/s/AKfycbxX1EInivW85EH1IGNZzh6bWJTLm6K9-ZRr3-wduHYFd34d1SW0dG2bAndm8CZvYPE/exec?action=guardarTarea&tarea=" + tareaOrg;
+                        // Log para verificar a resposta recebida
+                        Log.d("Resposta do Script", "Resposta recebida: " + response);
 
-                            StringRequest guardarTareaRequest = new StringRequest(Request.Method.GET, urlGuardarTarea, new Response.Listener<String>() {
-                                @Override
-                                public void onResponse(String response) {
-                                    if (response.equals("EXITO")) {
-                                        Toast.makeText(AccesoAdm_activity.this, "Tarea guardada exitosamente", Toast.LENGTH_SHORT).show();
-                                        verificarPermisoNotificaciones();
-                                    } else {
-                                        Toast.makeText(AccesoAdm_activity.this, "Error al guardar la tarea", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            }, new Response.ErrorListener() {
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-                                    Log.e("Error", error.toString());
-                                    Toast.makeText(AccesoAdm_activity.this, "Error al guardar la tarea", Toast.LENGTH_SHORT).show();
-                                }
-                            });
-
-                            requestQueue.add(guardarTareaRequest);
+                        // Garantir que a resposta não contenha espaços em branco extras
+                        if (response.trim().equalsIgnoreCase("true")) {
+                            // Se a clave é válida, prosseguir com a tarefa
+                            Intent intent = new Intent(getApplicationContext(), Evento_activity.class);
+                            startActivity(intent);
                         } else {
+                            Log.d("Resposta do Script", "Clave de administrador no válida: " + response);
                             Toast.makeText(AccesoAdm_activity.this, "Clave de administrador no válida", Toast.LENGTH_SHORT).show();
                         }
                     }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.e("Error", error.toString());
+                        Log.e("Error", "Erro ao verificar a clave: " + error.toString());
                         Toast.makeText(AccesoAdm_activity.this, "Error al verificar la clave", Toast.LENGTH_SHORT).show();
                     }
                 });
 
                 requestQueue.add(verificarClaveRequest);
+
+                // URL para guardar la tarea en Google Sheets
+                String urlGuardarTarea = "https://script.google.com/macros/s/AKfycbxX1EInivW85EH1IGNZzh6bWJTLm6K9-ZRr3-wduHYFd34d1SW0dG2bAndm8CZvYPE/exec?action=guardarTarea&tarea=" + tareaOrg + "&idInstalacion=" + idInstalacion;
+                StringRequest guardarTareaRequest = new StringRequest(Request.Method.GET, urlGuardarTarea, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response.trim().equals("EXITO")) {
+                            Toast.makeText(AccesoAdm_activity.this, "Tarea guardada exitosamente", Toast.LENGTH_SHORT).show();
+                            verificarPermisoNotificaciones();
+                        } else {
+                            Toast.makeText(AccesoAdm_activity.this, "Error al guardar la tarea", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Error", "Erro ao guardar a tarefa: " + error.toString());
+                        Toast.makeText(AccesoAdm_activity.this, "Error al guardar la tarea", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                requestQueue.add(guardarTareaRequest);
             }
         });
 
@@ -111,37 +130,34 @@ public class AccesoAdm_activity extends AppCompatActivity {
         });
     }
 
+    @SuppressLint("HardwareIds")
+    private void initializeIdInstalacion() {
+        idInstalacion = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+    }
+
     private void verificarPermisoNotificaciones() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // Verificar si el permiso de notificación está concedido
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                // Solicitar permiso de notificación
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, REQUEST_NOTIFICATION_PERMISSION);
             } else {
+                // Permiso ya concedido
                 configurarCanalNotificacion();
                 enviarNotificacion();
             }
         } else {
+            // No es necesario solicitar permisos en versiones anteriores a Android 13
             configurarCanalNotificacion();
             enviarNotificacion();
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == REQUEST_NOTIFICATION_PERMISSION) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                configurarCanalNotificacion();
-                enviarNotificacion();
-            } else {
-                Toast.makeText(this, "Permiso de notificación denegado", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
     private void configurarCanalNotificacion() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Canal de notificaciones", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                    "Canal de notificaciones", NotificationManager.IMPORTANCE_DEFAULT);
+            // Configurar otras propiedades del canal, si es necesario
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel);
         }
@@ -149,7 +165,7 @@ public class AccesoAdm_activity extends AppCompatActivity {
 
     private void enviarNotificacion() {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_notification)
+                .setSmallIcon(R.drawable.ic_notification) // Icono de la notificación
                 .setContentTitle("Tarea de Organización")
                 .setContentText("Tarea registrada exitosamente")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
@@ -160,7 +176,27 @@ public class AccesoAdm_activity extends AppCompatActivity {
         }
         notificationManager.notify(1, builder.build());
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initializeIdInstalacion();
+            } else {
+                Toast.makeText(this, "Permission denied to read phone state", Toast.LENGTH_SHORT).show();
+                idInstalacion = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+            }
+        }
+        if (requestCode == REQUEST_NOTIFICATION_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permiso concedido, configurar el canal de notificación
+                configurarCanalNotificacion();
+                enviarNotificacion();
+            } else {
+                // Permiso denegado
+                Toast.makeText(getApplicationContext(), "Permiso denegado", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
-
-
-
