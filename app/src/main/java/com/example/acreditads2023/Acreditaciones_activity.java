c7f1f9d7ebcac080494477e0ec6cbb0290d644c9
Rafael <rafa101993@gmail.com>
2024-07-05 18:23:40 -0300

    Update Acreditaciones_activity
    Ainda faltam alguns ajustes...

--- app/src/main/java/com/example/acreditads2023/Acreditaciones_activity.java
+++ app/src/main/java/com/example/acreditads2023/Acreditaciones_activity.java
@@ -1,10 +1,12 @@
 package com.example.acreditads2023;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -23,6 +25,11 @@ import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -37,6 +44,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 public class Acreditaciones_activity extends AppCompatActivity {
+    private ProgressDialog progressDialog;
     private PreviewView previewView;
     private ExecutorService cameraExecutor;
     private boolean isQRCodeDetected = false, isInside;
@@ -48,12 +56,15 @@ public class Acreditaciones_activity extends AppCompatActivity {
     public AcreditacionesDAO aDAO;
     private double LatitudPersona, LongitudPersona, NumLatitud, NumLongitud;
     private FusedLocationProviderClient fusedLocationProviderClient;
+    private RequestQueue requestQueue;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acreditaciones_activity);
+
+        requestQueue = Volley.newRequestQueue(this);
 
         previewView = findViewById(R.id.previewView);
         cameraExecutor = Executors.newSingleThreadExecutor();
@@ -156,12 +167,7 @@ public class Acreditaciones_activity extends AppCompatActivity {
                         for (Barcode barcode : barcodes) {
                             isQRCodeDetected = true;
                             rawValue = barcode.getRawValue();
-                            Acreditaciones a = new Acreditaciones();
-                            a.setTxQrCodeResult(rawValue);
-                            a.setTimeStamp(LocalDateTime.now().toString());
-                            aDAO.salvar(a);
                             handleQRCodeData(rawValue);
-                            btnAgregar.setVisibility(View.VISIBLE);
                             break; // Para o processamento de futuros códigos.
                         }
                     })
@@ -173,7 +179,14 @@ public class Acreditaciones_activity extends AppCompatActivity {
     private void handleQRCodeData(String data) {
         // Executar aqui as ações com os dados lidos do QR Code.
         // Mostrando um Toast apenas para demonstração do funcionamento.
-        Toast.makeText(this, "QR Code Data: " + data, Toast.LENGTH_LONG).show();
+
+        Acreditaciones a = new Acreditaciones();
+        a.setTxQrCodeResult(rawValue);
+        a.setTimeStamp(LocalDateTime.now().toString());
+        //aDAO.salvar(a);
+        btnAgregar.setVisibility(View.VISIBLE);
+
+        //Toast.makeText(this, "QR Code Data: " + data, Toast.LENGTH_LONG).show();
 
         // Finaliza a prévia da câmera depois de fazer a leitura do código
         stopCameraPreview();
@@ -181,6 +194,29 @@ public class Acreditaciones_activity extends AppCompatActivity {
         txQrCodeResult.setText("Dados do código: "+data + "\n\n Timestamp: "+LocalDateTime.now());
         previewView.setVisibility(View.INVISIBLE);
         txQrCodeResult.setVisibility(TextView.VISIBLE);
+
+        // Google Sheets URL
+        String urlSheets = "https://script.google.com/macros/s/AKfycbyQyPbRkjrPCyy8mLro7-yrRdFxVR5Tgzt7liTI2JrVx53enlPlbFtxvDXQLLfCliId/exec?action=buscarEventoPorCodigo"
+                + "&codigoQr=" + "18";
+
+        showBlockingDialog("Executando...");
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, urlSheets, new com.android.volley.Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                // Procesar la respuesta del servidor
+                dismissBlockingDialog();
+                retGetEvento(s);
+            }
+        }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(com.android.volley.VolleyError error) {
+                dismissBlockingDialog();
+                Log.e("Error", error.toString());
+                Toast.makeText(getApplicationContext(), "Evento no existe!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        requestQueue.add(stringRequest);
 
     }
     @Override
@@ -265,6 +301,28 @@ public class Acreditaciones_activity extends AppCompatActivity {
         // y settear los valores a estas dos variables
         Latitud = "-12.122345";
         Longitud = "0.000000";
+    }
+
+    protected void showBlockingDialog(String message) {
+
+        dismissBlockingDialog();
+        progressDialog = new ProgressDialog(this, ProgressDialog.THEME_DEVICE_DEFAULT_DARK);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage(message);
+        progressDialog.show();
+    }
+
+    protected void dismissBlockingDialog() {
+
+        if (progressDialog != null) {
+            progressDialog.dismiss();
+            progressDialog = null;
+        }
+    }
+
+    protected void retGetEvento(String s){
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+        Log.i("Retorno evento: ", s);
     }
 
 }
